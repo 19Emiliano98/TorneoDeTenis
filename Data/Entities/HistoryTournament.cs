4da045c86e62f8314ce3c9554f192202b85581a2
Emiliano Caballero <emiliano.caballero98@gmail.com>
2024-05-03 21:30:09 -0300

    Cree entidades

---
+++ Data/Entities/HistoryTournament.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class HistoryTournament
+    {
+        public int Id { get; set; }
+        public int IdPlayer { get; set; }
+        public int IdHistoryMatch { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Data/Entities/MatchHistory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class MatchHistory
+    {
+        public int Id { get; set; }
+        public int IdTournament { get; set; }
+        public int IdMatch { get; set; }
+    }
+}
