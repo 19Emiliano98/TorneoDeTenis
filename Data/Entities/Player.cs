04b256a45ae0fdb4d1fe67209263d7ebdb40de43
Emiliano Caballero <emiliano.caballero98@gmail.com>
2024-05-03 21:25:25 -0300

    Creo las capas y corrijo, empeze con el armado de entidades

---
+++ Data/Entities/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Luck { get; set; }
+        public int Strenght { get; set; }
+        public int Speed { get; set; }
+    }
+}
