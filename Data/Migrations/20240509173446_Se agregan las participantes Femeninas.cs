53792636ab63a84a8724a5a8714a5c5baba279e9
Emiliano Caballero <emiliano.caballero98@gmail.com>
2024-05-09 14:53:04 -0300

    Realizo las migraciones correspondientes y adjunto los archivos sql

---
+++ Data/Migrations/20240509173446_Se agregan las participantes Femeninas.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeagreganlasparticipantesFemeninas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "Player",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TimeReaction",
+                table: "Player",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Player");
+
+            migrationBuilder.DropColumn(
+                name: "TimeReaction",
+                table: "Player");
+        }
+    }
+}
