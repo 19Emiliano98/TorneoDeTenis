c574211d2bcee8cac0b9f3e332ff25565e40c3ba
Emiliano Caballero <emiliano.caballero98@gmail.com>
2024-05-17 16:24:03 -0300

    Logro solucionar los problemas que tenia con el testing incorporando la libreria de EF InMEmory

--- UnitTesting/PlayerTesting.cs
+++ UnitTesting/PlayerTesting.cs
@@ -1,12 +1,14 @@
 ﻿using Contracts.Exceptions;
 using Data.Entities;
 using Data.Repository;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using Services.Interfaces;
 using Services.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,52 +16,160 @@ namespace UnitTesting
 {
     public class PlayerTesting
     {
-        // No utilizo Moq por el simple hecho de que no me permite trabajar con 
-        // la DB, podria utilizar la libreria inMemory para simular una DB y hacerlo
-        // mucho mas dinamico pero para este proyecto al ser chico no es necesario
-
-        private readonly TournamentContext? _context;
-        private readonly PlayerService _playerService;
+        private readonly DbContextOptions<TournamentContext> _options;
 
         public PlayerTesting()
+        { 
+            _options = new DbContextOptionsBuilder<TournamentContext>()
+                .UseInMemoryDatabase(databaseName: "Database")
+                .Options;
+        }
+
+        private TournamentContext CreateContext()
         {
-            _playerService = new PlayerService(_context);
+            var context = new TournamentContext(_options);
+            return context;
+        }
+
+        private static void SeedDatabase(TournamentContext context)
+        {
+            context.Set<Player>().Add(new Player
+            {
+                Id = 1,
+                Name = "Carlos",
+                Luck = 50,
+                Hability = 30,
+                Strenght = 30,
+                Speed = 20,
+                TimeReaction = 20,
+                Gender = "Male"
+            });
+            context.Set<Player>().Add(new Player
+            {
+                Id = 2,
+                Name = "Emiliano",
+                Luck = 50,
+                Hability = 30,
+                Strenght = 30,
+                Speed = 20,
+                TimeReaction = 20,
+                Gender = "Male"
+            });
+            context.Set<Player>().Add(new Player
+            {
+                Id = 3,
+                Name = "Jorge",
+                Luck = 50,
+                Hability = 30,
+                Strenght = 30,
+                Speed = 20,
+                TimeReaction = 20,
+                Gender = "Male"
+            });
+            context.Set<Player>().Add(new Player
+            {
+                Id = 4,
+                Name = "Lucas",
+                Luck = 50,
+                Hability = 30,
+                Strenght = 30,
+                Speed = 20,
+                TimeReaction = 20,
+                Gender = "Male"
+            });
+
+            context.Set<Player>().Add(new Player
+            {
+                Id = 5,
+                Name = "Lucrecia",
+                Luck = 50,
+                Hability = 30,
+                Strenght = 30,
+                Speed = 20,
+                TimeReaction = 20,
+                Gender = "Female"
+            });
+            context.Set<Player>().Add(new Player
+            {
+                Id = 6,
+                Name = "Paola",
+                Luck = 50,
+                Hability = 30,
+                Strenght = 30,
+                Speed = 20,
+                TimeReaction = 20,
+                Gender = "Female"
+            });
+            context.Set<Player>().Add(new Player
+            {
+                Id = 7,
+                Name = "Jorgelina",
+                Luck = 50,
+                Hability = 30,
+                Strenght = 30,
+                Speed = 20,
+                TimeReaction = 20,
+                Gender = "Female"
+            });
+
+            context.SaveChanges();
+        }
+        private static void ClearDatabase(TournamentContext context)
+        {
+            context.Set<Player>().RemoveRange(context.Set<Player>());
+            context.SaveChanges();
         }
 
         [Fact]
-        public void SetLuckAsync_ThrowNotFound()
+        public async Task SetLuckAsync_ThrowNotFound()
         {
             // Arrange
-            var gender = "Unknown";
+            var context = CreateContext();
+            var playerService = new PlayerService(context);
+            var gender = "Uknown";
 
             // Act && Asserts
-            var exception = Assert.ThrowsAsync<NotFoundException>(async () => await _playerService.SetLuckAsync(gender));
+            var exception = await Assert.ThrowsAsync<NotFoundException>(async () => await playerService.SetLuckAsync(gender));
+        }
+
+        [Fact]
+        public async Task SetLuckAsync_ThrowBadRequest()
+        {
+            // Arrange
+            var context = CreateContext();
+
+            ClearDatabase(context);
+            SeedDatabase(context);
+            var gender = "Female";
+
+            var playersList = context.Set<Player>()
+                                    .Where(x => x.Gender == gender)
+                                    .ToList();
+
+            var playerService = new PlayerService(context);
+
+            // Act && Asserts
+            var exception = await Assert.ThrowsAsync<BadRequestException>(async () => await playerService.SetLuckAsync(gender));
         }
 
         [Fact]
         public void CheckAmountOfPlayers_QuantityAcepted()
         {
             // Arrange
-            var playersList = new List<Player>();
-            var player = new Player
-            {
-                Id = 1,
-                Name = "Emiliano",
-                Luck = 40,
-                Hability = 20,
-                Strenght = 50,
-                Speed = 40,
-                TimeReaction = 30,
-                Gender = "Male"
-            };
+            var context = CreateContext();
+            
+            ClearDatabase(context);
+            SeedDatabase(context);
+            var gender = "Male";
 
-            for (int i = 0; i < 8; i++)
-            {
-                playersList.Add(player);
-            }
+            var playersList = context.Set<Player>()
+                                    .Where(x => x.Gender == gender)
+                                    .ToList();
+
+            var playerService = new PlayerService(context);
 
             // Act
-            var result = _playerService.CheckAmountOfPlayers(playersList);
+            var result = playerService.CheckAmountOfPlayers(playersList);
 
             // Asserts
             Assert.True(result);
@@ -69,26 +179,20 @@ namespace UnitTesting
         public void CheckAmountOfPlayers_QuantityIsNotAcepted()
         {
             // Arrange
-            var playersList = new List<Player>();
-            var player = new Player
-            {
-                Id = 1,
-                Name = "Emiliano",
-                Luck = 40,
-                Hability = 20,
-                Strenght = 50,
-                Speed = 40,
-                TimeReaction = 30,
-                Gender = "Male"
-            };
+            var context = CreateContext();
 
-            for (int i = 0; i < 3; i++)
-            {
-                playersList.Add(player);
-            }
+            ClearDatabase(context);
+            SeedDatabase(context);
+            var gender = "Female";
+
+            var playersList = context.Set<Player>()
+                                    .Where(x => x.Gender == gender)
+                                    .ToList();
+
+            var playerService = new PlayerService(context);
 
             // Act
-            var result = _playerService.CheckAmountOfPlayers(playersList);
+            var result = playerService.CheckAmountOfPlayers(playersList);
 
             // Asserts
             Assert.False(result);
